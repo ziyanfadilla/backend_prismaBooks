68389dcc27a3a7f964c0929ba99d751cd65f3278
ziyanfadilla <ziyanfadilla1@gmail.com>
2021-03-20 14:01:32 +0700

    nambah static public dan fix port

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ require('dotenv').config({});
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3001
 
 const mainRouters = require('./src/routers');
  
-
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded ({extended : false}));
 app.use('/', mainRouters);
 
 
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log("server berjalan");
 });
